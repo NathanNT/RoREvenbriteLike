42facaf5998a23fa10bc4a89d21b501679591dc3
Nathan <nathancpt1@gmail.com>
2019-11-06 03:05:15 +0100

    Updates for heroku deployment

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,72 +1,24 @@
-class EventsController < ApplicationController
-  before_action :set_event, only: [:show, :edit, :update, :destroy]
-
-  # GET /events
-  # GET /events.json
+  class EventsController < ApplicationController
+    include ApplicationHelper
+  before_action :authenticate_user, except: [:index, :show]
   def index
     @events = Event.all
-    #figure out how to find a featured event and what it is
   end
-
-  # GET /events/1
-  # GET /events/1.json
   def show
+    @event= Event.find(params[:id])
   end
-
-  # GET /events/new
   def new
-    @event = Event.new
   end
-
-  # GET /events/1/edit
-  def edit
-  end
-
-  # POST /events
-  # POST /events.json
   def create
-    @event = Event.new(event_params)
-
-      if @event.save
-        redirect_to @event, notice: 'Event was successfully created.'
-      else
-         render :new
-      end
-    
-  end
-
-  # PATCH/PUT /events/1
-  # PATCH/PUT /events/1.json
-  def update
-    respond_to do |format|
-      if @event.update(event_params)
-        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
-        format.json { render :show, status: :ok, location: @event }
-      else
-        format.html { render :edit }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
+    @event=Event.new(title: params[:title],price:params[:price],description:params[:description],admin_id:current_user.id,location: params[:location],start_date:params[:start_date],duration:params[:duration])
+    if @event.save
+      redirect_to event_path(@event.id)
+    else
+      redirect_to new_event_path
     end
   end
-
-  # DELETE /events/1
-  # DELETE /events/1.json
-  def destroy
-    @event.destroy
-    respond_to do |format|
-      format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+  def edit
+    @event=Event.update(title: params[:title],price:params[:price],description:params[:description],admin_id:current_user.id,location: params[:location],start_date:params[:start_date],duration:params[:duration])
+    redirect_to event_path(@event.id)
   end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_event
-      @event = Event.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def event_params
-      params.permit!.fetch(:event)
-    end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def admin (event)
+		@user=User.find(event.admin_id)
+	end
+	def authenticate_user
+  	unless current_user
+	  redirect_to new_user_session_path
+	end
+  end
 end
