1c1d47a56fb89784f56e28f61186b00797350ef3
Nathan <nathancpt1@gmail.com>
2019-11-06 01:40:13 +0100

    Updates for heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  root 'events#index'
   devise_for :users
-  resources :attendances
-  resources :events
-  resources :team, only: [:index]
-  resources :contact, only: [:index]
-
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  resources :event
 end
